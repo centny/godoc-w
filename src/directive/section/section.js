a78bf103706d34acec2ec791ecaf07424d9ed556
Centny <centny@gmail.com>
2016-04-26 16:13:43 +0800

    fix the comment is not newline bug.

--- src/directive/section/section.js
+++ src/directive/section/section.js
@@ -7,7 +7,7 @@ angular.module('app').directive("sectionDir", function() {
         scope: {
             data: "="
         },
-        controller: function($scope, $element, $timeout) {
+        controller: function($scope, $element, $timeout, $sce) {
             $scope.replace = function(obj) {
                 return obj.replace(/[\/\.]/g, "_");
             };
@@ -24,11 +24,17 @@ angular.module('app').directive("sectionDir", function() {
                 if (sch) {
                     sch += '#';
                 }
-                console.log(item);
                 window.location.href = (sch || '/#') + (item.pkg + "/").replace(/[\/\.]+/g, "_") + item.name;
             };
             $scope.is_obj = function(item) {
                 return typeof item == "object";
+            };
+            $scope.repl_ws = function(item) {
+                if (item) {
+                    return $sce.trustAsHtml(item.replace("\n", "<br/>"));
+                } else {
+                    return "";
+                }
             };
         }
     };
